75b0e2f6176387182887da71e0018974718021fe
151610209-smkassalaam <151610209@smkassalaam.sch.id>
2017-07-18 11:57:12 +0700

    LARAGAME

---
+++ resources/views/layouts/slide.blade.php
@@ -0,0 +1,51 @@
+
+   <div class="container-fluid">		
+	<br/>
+	<div class="col-md-10  col-md-offset-1">
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>		
+			</ol>
+
+			<!-- deklarasi carousel -->
+			 <div class="carousel-inner carousel" role="listbox">
+				<div class="item active">
+					<img src="{{asset('/dota1.jpg')}}" alt="">
+					<div class="carousel-caption">
+						<h3>www.dota2.com</h3>
+						<p>Update terbatu DOTA 2</p>
+					</div>
+				</div>
+				<div class="item">
+					<img src="/bf1.jpg" alt="www.malasngoding.com">
+					<div class="carousel-caption">
+						<h3>Battle Filed 1 Realise Now!</h3>
+						<p>-</p>
+					</div>
+				</div> 
+				-->
+				<div class="item active">
+					<img src="/Nier.jpg" alt="www.malasngoding.com">
+					<div class="carousel-caption">
+						<h3>Tutorial Android</h3>
+						<p>Tutorial membuat aplikasi android.</p>
+					</div>
+				</div>				
+			</div>
+
+			<!-- membuat panah next dan previous -->
+			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
+   </div>
+<br>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+@include('layouts.slide') 
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel" style="background-color: rgba(255,255,255,0.9);" >
+                <div class="panel-heading">
+                    Heloo
+                </div>
+                <div class="panel-body">
+                    Selamat Datang Di Laragame
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
